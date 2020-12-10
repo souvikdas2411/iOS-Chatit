8f0d6852fa17379760d9e9822266a3d95f51adcb
souvikdas2411 <souvikdas2411@gmail.com>
2020-12-11 00:46:51 +0530

    stable commit

--- Chatit/NewConversationViewController.swift
+++ Chatit/NewConversationViewController.swift
@@ -13,12 +13,26 @@ class NewConversationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Looks for single or multiple taps.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+        
+        searchBar.delegate = self
+        searchBar.becomeFirstResponder()     
     }
     
-
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 
 }
 extension NewConversationViewController: UISearchBarDelegate{
-    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
 }
